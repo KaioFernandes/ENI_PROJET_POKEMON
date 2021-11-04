66537442209233f6618bc438aba9aebaa2e7e903
AD\kfernandes2021 <kaio.fernandesdiascordeiro2021@campus-eni.fr>
2021-11-04 12:09:58 +0100

    initial commit + sql script

---
+++ Create_Tables_Pokemon.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Capacities (
+	id int not null primary key identity(1,1),
+	name varchar(255) not null,
+	power int not null,
+	type varchar(255) not null
+);
+
+CREATE TABLE Pokemon (
+	id int not null primary key identity(1,1),
+	name varchar(255) not null,
+	life_points int not null,
+	attack_strength int not null,
+	defence_strength int not null,
+	speed int not null,
+	type varchar(255) not null,
+	capacity int not null,
+	CONSTRAINT FK_Capacity_Pokemon FOREIGN KEY (capacity) REFERENCES Capacities(id)
+
+);

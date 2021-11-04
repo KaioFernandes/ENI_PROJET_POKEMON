cc8fa7673cc96148c348261cb30e73406edc076c
AD\jhamadi2020 <jana.hamadi2020@campus-eni.fr>
2021-11-04 15:19:08 +0100

    pages home, capacity

---
+++ ENI_PROJET_POKEMON/src/main/java/servlets/CapacityServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CapacityServlet
+ */
+@WebServlet("/capacity")
+public class CapacityServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public CapacityServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/capacity.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ENI_PROJET_POKEMON/src/main/webapp/WEB-INF/capacity.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title><fmt:message key="msg.capacity.title"></fmt:message></title>
+</head>
+<body>
+	<c:import url="header.jsp"></c:import>
+	
+	<h2>
+		<fmt:message key="msg.capacity.headline"></fmt:message>
+	</h2>
+	<form action="POST">
+		
+	
+	</form>
+</body>
+</html>
